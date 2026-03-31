3ecec836a7fe23a284b967613c540fd3e2fac0ec
Mirjana Ristic <mima34.mg@gmail.com>
2026-04-01 00:33:06 +0200

    Add UI menu and update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace DataVerseManager
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using DataVerseManager.Models;
+using DataVerseManager.Services;
+using DataVerseManager.UI;
+
+// load existing expenses from json file on startup
+var jsonService = new JsonService();
+var store = new DataStore<Expense>();
+store.SetAll(jsonService.Load());
+
+// start the main menu
+var menu = new MenuDisplay(store, jsonService);
+menu.ShowMainMenu();
