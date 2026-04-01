4dc31d70f209a79b4ad07c150567e09c1d45d0f1
Mirjana Ristic <mima34.mg@gmail.com>
2026-04-01 03:22:29 +0200

    Add loading spinner on startup

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 ﻿using DataVerseManager.Models;
 using DataVerseManager.Services;
 using DataVerseManager.UI;
+using Spectre.Console;
 
-// load existing expenses from json file on startup
+// show a spinner while loading data
 var jsonService = new JsonService();
 var store = new DataStore<Expense>();
-store.SetAll(jsonService.Load());
+
+AnsiConsole.Status()
+    .Start("Loading data...", ctx =>
+    {
+        ctx.Spinner(Spinner.Known.Dots);
+        ctx.SpinnerStyle(Style.Parse("green"));
+        store.SetAll(jsonService.Load());
+    });
 
 // start the main menu
 var menu = new MenuDisplay(store, jsonService);
